faa7014108286f51e98f18fed3ddc9f15dbaf21e
zubairwardak <zubair.wardak113@gmail.com>
2026-06-02 10:20:17 +0430

    Video Capturing

---
+++ videoCapture.py
@@ -0,0 +1,14 @@
+import cv2
+
+video = cv2.VideoCapture('nvidoe.mp4')
+
+while True:
+    ret,frame = video.read()
+    if not ret:
+        break
+    cv2.imshow("Video Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
